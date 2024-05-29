8651e938d9e8a9316963571f252cbd5361eab83f
Deepanshu P <deepanshup175@gmail.com>
2024-05-29 23:22:04 +0530

    Day 6

---
+++ Array/Medium/Container With Most Water.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int n = height.size();
+        int maxwater=0;
+        int i = 0;
+        int j = n-1;
+        while(i < j)
+        {
+            int w = j-i;
+            int h = min(height[i],height[j]);
+            int area = w*h;
+            maxwater = max(maxwater,area);
+            if(height[i] > height[j])
+            {
+                j--;
+            }
+            else
+            {
+                i++;
+            }
+        }
+        return maxwater;
+    }
+};

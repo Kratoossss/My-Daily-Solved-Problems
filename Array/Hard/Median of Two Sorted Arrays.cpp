66be80d658d9dd4a9c8cfe3a7bd6707f8ad72bb9
Deepanshu P <deepanshup175@gmail.com>
2024-06-08 06:32:58 +0530

    Day 16

---
+++ Array/Hard/Median of Two Sorted Arrays.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        vector<int> temp;
+        float sum;
+        for(int i=0 ; i<nums1.size() ; i++)
+        {
+            temp.push_back(nums1[i]);
+        }
+        for(int i=0 ; i<nums2.size() ; i++)
+        {
+            temp.push_back(nums2[i]);
+        }
+        sort(temp.begin(),temp.end());
+        int n = temp.size();
+        int start = 0;
+        int end = n-1;
+        float mid = (end - start)/2;
+        if(n%2 == 1)
+        {
+            return temp[mid];
+        }
+        else
+        {
+            float sum = (float)(temp[mid]+temp[mid+1])/2;
+            return sum;
+        }
+        return{};
+    }
+};

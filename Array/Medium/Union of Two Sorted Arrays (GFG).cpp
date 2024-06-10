5f84f3b6421e4fa83456998a248fccb6e97fa1e3
Deepanshu P <deepanshup175@gmail.com>
2024-06-10 13:38:33 +0530

    Day 18

---
+++ Array/Medium/Union of Two Sorted Arrays (GFG).cpp
@@ -0,0 +1,24 @@
+class Solution{
+    public:
+    //arr1,arr2 : the arrays
+    // n, m: size of arrays
+    //Function to return a list containing the union of the two arrays. 
+    vector<int> findUnion(int arr1[], int arr2[], int n, int m)
+    {
+        vector<int> ans;
+        set<int> s;
+        for(int i=0 ; i<n ; i++)
+        {
+            s.insert(arr1[i]);
+        }
+        for(int j=0 ; j<m ; j++)
+        {
+            s.insert(arr2[j]);
+        }
+        for(auto i:s)
+        {
+            ans.push_back(i);
+        }
+        return ans;
+    }
+};

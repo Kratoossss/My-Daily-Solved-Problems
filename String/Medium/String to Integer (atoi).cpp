8ce649ba5a8da1a6c972ec4b09434dd6113164ac
Deepanshu P <deepanshup175@gmail.com>
2024-07-06 00:48:44 +0530

    Day 43 (Good Question)

---
+++ String/Medium/String to Integer (atoi).cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int myAtoi(string s) {
+        int i = 0;
+        int sign = 1;
+        long ans = 0;
+        while(i<s.size() && s[i] == ' ')
+        {
+            i++;
+        }
+        if(s[i] == '-')
+        {
+            sign = -1;
+            i++;
+        }
+        else if(s[i] == '+')
+        {
+            sign = 1;
+            i++;
+        }
+        while(i < s.length())
+        {
+            if(s[i]>='0' && s[i]<='9')
+            {
+                ans = ans*10+(s[i]-'0');
+                if(ans>INT_MAX && sign == -1)
+                {
+                    return INT_MIN;
+                }
+                else if (ans>INT_MAX && sign == 1)
+                {
+                    return INT_MAX;
+                }
+                i++;
+            }
+            else
+            {
+                return ans*sign;
+            }
+        }
+        return sign*ans;
+    }
+};

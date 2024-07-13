4ac9f0f1bf0f961eb2d78675f0baadf8180d99ad
Deepanshu P <deepanshup175@gmail.com>
2024-07-14 00:15:48 +0530

    Day 52

---
+++ String/Easy/Find the Difference.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        unordered_map<char,int> mp;
+        for(int i=0 ; i<s.size() ; i++)
+        {
+            mp[s[i]]++;
+        }
+        for(int i=0 ; i<t.size() ; i++)
+        {
+            mp[t[i]]++;
+        }
+        for(auto i:mp)
+        {
+            if(i.second%2 != 0)
+            {
+                return i.first;
+            }
+        }
+        return 0;
+    }
+};
